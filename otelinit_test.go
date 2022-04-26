051f4ac0048f3e749e93bafb8b4863d3a85d32b8
Vincent Serpoul <vincent@serpoul.com>
2022-04-26 23:29:35 +0800

    docs: add examples

--- otelinit_test.go
+++ otelinit_test.go
@@ -5,7 +5,9 @@ import (
 	"bytes"
 	"context"
 	"errors"
+	"fmt"
 	"io"
+	"log"
 	"testing"
 	"time"
 
@@ -93,4 +95,52 @@ func TestInitProvider(t *testing.T) {
 			}
 		})
 	}
+}
+
+// Initialize a otel provider with a collector
+func Example_initProviderCollector() {
+	ctx := context.Background()
+
+	shutdown, err := InitProvider(
+		ctx,
+		"simple-gohttp",
+		WithGRPCTraceExporter(
+			ctx,
+			fmt.Sprintf("%s:%d", "127.0.0.1", 4317),
+		),
+	)
+	if err != nil {
+		log.Println("failed to initialize opentelemetry")
+
+		return
+	}
+
+	defer func() {
+		if err := shutdown(); err != nil {
+			log.Println("failed to shutdown")
+		}
+	}()
+}
+
+// Initialize a otel provider and discard traces
+// useful for dev
+func Example_initProviderDiscardTraces() {
+	ctx := context.Background()
+
+	shutdown, err := InitProvider(
+		ctx,
+		"simple-gohttp",
+		WithWriterTraceExporter(io.Discard),
+	)
+	if err != nil {
+		log.Println("failed to initialize opentelemetry")
+
+		return
+	}
+
+	defer func() {
+		if err := shutdown(); err != nil {
+			log.Println("failed to shutdown")
+		}
+	}()
 }
