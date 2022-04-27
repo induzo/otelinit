0032b96b617065ee551843a6f5fcdb15389a387a
github-action-benchmark <github@users.noreply.github.com>
2022-04-27 03:36:59 +0000

    add Benchmark (go) benchmark result for e5d33b97cd43d72ca77fc8874c15a1148ad986b1

---
+++ data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1651030619094,
+  "repoUrl": "https://github.com/induzo/otelinit",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "distinct": true,
+          "id": "e5d33b97cd43d72ca77fc8874c15a1148ad986b1",
+          "message": "fix: change data dir path",
+          "timestamp": "2022-04-27T11:36:08+08:00",
+          "tree_id": "5584c4cc27c0e0e1fb5d884187f95600a5a9d440",
+          "url": "https://github.com/induzo/otelinit/commit/e5d33b97cd43d72ca77fc8874c15a1148ad986b1"
+        },
+        "date": 1651030618142,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkInitProvider",
+            "value": 43761,
+            "unit": "ns/op\t   48666 B/op\t      56 allocs/op",
+            "extra": "32851 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
